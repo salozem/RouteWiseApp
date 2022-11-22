9ed06484c3b3dfca1071b3904340179da1904f72
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-11-22 16:04:56 -0500

    feat(dev): implemented adjacency list for ui based on traffic factor

--- utils/adjacencyListGenerator.py
+++ utils/adjacencyListGenerator.py
@@ -2,24 +2,26 @@ from datetime import datetime
 import json
 import math
 
+
 def generateAdjacencyList(listPath, streetPath):
-    adjacencyList={}
-    current_time=datetime.now().hour
-    street={}
-    keyValue={}
+    adjacencyList = {}
+    current_time = datetime.now().hour
+    street = {}
+    keyValue = {}
 
     def initializeData():
         with open(listPath, mode="r", encoding="utf-8") as f:
             nonlocal adjacencyList
             adjacencyList = json.load(f)
-            adjacencyList = [(int(key), value) for key, value in adjacencyList.items()]
+            adjacencyList = [(int(key), value)
+                             for key, value in adjacencyList.items()]
             adjacencyList = sorted(adjacencyList, key=lambda x: x[0])
 
             i = 0
             for key, val in adjacencyList:
                 keyValue[key] = i
                 i += 1
-        
+
         with open(streetPath, mode="r", encoding="utf-8") as file:
             nonlocal street
             streetList = json.load(file)
@@ -31,24 +33,28 @@ def generateAdjacencyList(listPath, streetPath):
                 value = element[v]
 
                 street[key] = value
-    
+
     def trafficHour(time):
-        fact = round(5 * math.cos(2 * (time / 3) - 1) * math.sin((time / 6) - 2) +5, 3)
-        if fact > 10: return 10
-        if fact < 0 : return 0
+        fact = round(5 * math.cos(2 * (time / 3) - 1)
+                     * math.sin((time / 6) - 2) + 5, 3)
+        if fact > 10:
+            return 10
+        if fact < 0:
+            return 0
+
         return fact
 
     def calcTrafficFactor(val, time):
         value = val * trafficHour(time)
         return value / 10
 
-    def calcWeight(val , length, time):
+    def calcWeight(val, length, time):
         return calcTrafficFactor(val, time) * math.log10(length)
-    
-    def getWeight(city1, city2):
-        str = street[(city1, city2)]
-        val = str["val"]
-        length = str["length"]
+
+    def getWeight(pos1, pos2):
+        lima_street = street[(pos1, pos2)]
+        val = lima_street["val"]
+        length = lima_street["length"]
         return calcWeight(val, length, current_time)
 
     def newAdjacencyList():
@@ -57,16 +63,19 @@ def generateAdjacencyList(listPath, streetPath):
 
         newAdjacencyList = []
         for key, arr in adjacencyList:
-            newAdjacencyList.append(list(map(lambda x: (keyValue[x], getWeight(key, x)), arr)))
+            newAdjacencyList.append(
+                list(map(lambda x: (keyValue[x], getWeight(key, x)), arr)))
         return newAdjacencyList
 
-        initializeData()
-        return newAdjacencyList()
+    initializeData()
+    return newAdjacencyList()
+
 
 def generateListForUI(interPath):
     with open(interPath) as file:
         data = json.load(file)
-        data = [(int(key), (value['x'], value['y'])) for key, value in data.items()]
+        data = [(int(key), (value['x'], value['y']))
+                for key, value in data.items()]
         data = sorted(data, key=lambda x: x[0])
 
     locations = []
