ed96c3d6481d43fd1f1204332579e416e6983511
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-11-22 22:38:18 -0500

    fix(dev): reformat to clean up code

--- utils/graph_lima.py
+++ utils/graph_lima.py
@@ -1,8 +1,8 @@
 import json
 
 adjacencyListRoute = "../dto/lima_adjacency_list.json"
-streetsRoute = "../dto/lima_district_streets.json"
-intersectionsRoute = "../dto/intersecciones.json"
+streetsRoute = "../dto/lima_streets.json"
+intersectionsRoute = "../dto/intersections.json"
 
 
 class cityGraph:
@@ -11,13 +11,13 @@ class cityGraph:
         self.streets = {}
         self.intersections = {}
 
-        self.initGraph(adjacencyListRoute, streetsRoute, intersectionsRoute)
+        self.init_graph(adjacencyListRoute, streetsRoute, intersectionsRoute)
 
     def init_graph(self, adjacencyListRoute, streetsRoute, intersectionsRoute):
         with open(adjacencyListRoute, mode="r", encoding="utf-8") as f:
             self.adjacencyList = json.load(f)
 
-        with open(streetsRoute, mode="r", enconding="utf-8") as f:
+        with open(streetsRoute, mode="r", encoding="utf-8") as f:
             _streets = json.load(f)
             for e in _streets:
                 k, v = e
@@ -25,7 +25,5 @@ class cityGraph:
                 value = e[v]
 
                 self.streets[key] = value
-        with open(intersectionRoute, mode="r", enconding="utf-8") as f:
+        with open(intersectionsRoute, mode="r", encoding="utf-8") as f:
             self.intersections = json.load(f)
-
-    # def set_weight():
